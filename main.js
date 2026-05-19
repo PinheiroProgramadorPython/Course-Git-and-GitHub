aeb4bd6c362dba1b8c7a738f2ce735605cd6b939
PinheiroProgramadorPython <pinheiroprogramadorpython@gmail.com>
2026-05-19 11:01:53 -0300

    Create Link GitHub

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 document.addEventListener(("DOMContentLoaded"), () => {
-    alert("Config o JavaScript")
+    const variavel = false;
+    const linkGitHub = document.createElement("a"); 
+    linkGitHub.href = "https://github.com/PinheiroProgramadorPython/";
+    linkGitHub.textContent = variavel ? "GitHub" :
+        "https://github.com/PinheiroProgramadorPython/";
+    linkGitHub.target = "_blank"
+    const section = document.querySelector("section");
+    section.append(linkGitHub);
+    alert("Configuração do JavaScript concluída! Link adicionado.");
 })
